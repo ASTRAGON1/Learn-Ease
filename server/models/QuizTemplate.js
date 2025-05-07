9cd01cc7478d758ae5a3bcc13a318a4dd4419f3a
ILIASS <ibahtaine@gmail.com>
2025-05-08 01:14:16 +0300

    changed comment

--- server/models/QuizTemplate.js
+++ server/models/QuizTemplate.js
@@ -9,7 +9,7 @@ const questionSchema = new mongoose.Schema({
 const quizTemplateSchema = new mongoose.Schema({
   title: String,
   course_id: String,
-  createdBy: String, // teacher_id or "AI"
+  createdBy: String, // teacher_id or "AI, even if this attributes isnt available in the er diagram, ze can add it to the quiz template"
   questions: [questionSchema],
   createdAt: { type: Date, default: Date.now }
 });
