c54a905c08f3e583e1fe9f909f11275f2cbc54c7
fo2sh1999 <141173611+fo2sh1999@users.noreply.github.com>
2025-05-07 21:41:55 +0300

    quizController.js

---
+++ server/controllers/quizController.js
@@ -0,0 +1,30 @@
+const Quiz = require("../models/Quiz");
+
+exports.createQuiz = async (req, res) => {
+  try {
+    const quiz = new Quiz(req.body);
+    await quiz.save();
+    res.status(201).json(quiz);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+exports.getAllQuizzes = async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+    res.json(quizzes);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getQuizById = async (req, res) => {
+  try {
+    const quiz = await Quiz.findById(req.params.id);
+    if (!quiz) return res.status(404).json({ message: "Quiz not found" });
+    res.json(quiz);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
