5ffbac70d47a2b37abd8208def7d99e0204696bf
Muhammed <belloalamin10@outlook.com>
2025-08-26 19:42:01 +0100

    New updates

--- server/models/StudentCourse.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const studentCourseSchema = new mongoose.Schema({
-  student: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
-  course: { type: mongoose.Schema.Types.ObjectId, ref: "Course", required: true },
-  enrolledAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("StudentCourse", studentCourseSchema);
