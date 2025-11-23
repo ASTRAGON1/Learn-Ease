b4a48476fb5badc95261c5e7bf592d475a4a5323
ASTRAGON01 <ibahtaine@gmail.com>
2025-11-24 00:10:00 +0200

    Added MongoDB data and collections

--- server/models/Courses.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const CourseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    contentIds: [String],
-    category: String,
-});
-
-module.exports = mongoose.model('course', CourseSchema, 'Courses');
